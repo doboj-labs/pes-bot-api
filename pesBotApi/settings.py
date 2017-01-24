38c984508e3f64a9fb58f06d328305ad4f1cf752
ognjetina <ognjen.lazic@codecentric.de>
2017-01-24 19:17:51 +0100

    Fix static files root

--- pesBotApi/settings.py
+++ pesBotApi/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATIC_ROOT = os.path.abspath('staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'staticfiles'))
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
