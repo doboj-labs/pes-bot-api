18756c20db84ed18de1ef0d248bda1a8b840c94f
ognjetina <ognjen.lazic@codecentric.de>
2017-01-24 19:57:34 +0100

    Add localhost to allowed hosts

--- pesBotApi/settings.py
+++ pesBotApi/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'),'127.0.0.1']
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
