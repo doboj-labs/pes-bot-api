c96e80dbe9b0efd662efcb9f538b6cac7349c941
ognjetina <ognjen.lazic@codecentric.de>
2017-01-24 21:21:37 +0100

    Add fixtures for teams Data

--- api/models.py
+++ api/models.py
@@ -14,6 +14,7 @@ class Tournament(models.Model):
 
 class Team(models.Model):
     name = models.CharField(max_length=200)
+    real_name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
