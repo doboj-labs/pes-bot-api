827e6dd8ce945c821876cac98791f95859cd1bd4
ognjetina <ognjen.lazic@codecentric.de>
2017-01-25 10:17:06 +0100

    Change get match controller

--- api/fixtures/namesToJson.py
+++ api/fixtures/namesToJson.py
@@ -4,8 +4,8 @@ with open('data.txt') as fp:
 
         for idx, line in enumerate(fp):
             parts = line.split('-')
-            write_this = '''{"model": "api.team","pk": %s,"fields": {"name": "%s","real_name": "%s"}},''' % (
-                idx + 1, parts[0], parts[1].strip())
+            write_this = '''{"model": "api.team","pk": %s,"fields": {"name": "%s","real_name": "%s","slug": "%s"}},''' % (
+                idx + 1, parts[0], parts[1].strip(), parts[0].replace(" ", "")[:3])
 
             json_file.writelines(write_this.rstrip())
 
