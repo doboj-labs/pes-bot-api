5935a3af3c1ea6e480c58c93c6a8ff490593c06d
ognjetina <ognjen.lazic@codecentric.de>
2017-02-06 14:37:44 +0100

    Change user UI

--- pes_bot/settings.py
+++ pes_bot/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), '127.0.0.1']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), '127.0.0.1', '192.168.1.128']
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
