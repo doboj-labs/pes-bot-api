c275a15ca5859854af287446b8180a50baa1b9c1
ognjetina <ognjen.lazic@codecentric.de>
2017-02-08 08:45:20 +0100

    Url fix. Seperate matches by dayes.

--- pes_bot/urls.py
+++ pes_bot/urls.py
@@ -9,13 +9,14 @@ router.register(r'Match', views.MatchViewSet)
 
 urlpatterns = [
 
-    url(r'^web/tables', views.table),
-    url(r'^web/matches', views.show_all_matches),
-    url(r'^web/', views.main_web),
     url(r'^admin/', admin.site.urls),
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^get-next-match$', views.get_next_match),
+    url(r'^get-next-match', views.get_next_match),
     url(r'^start-stop-match', views.start_stop_match),
     url(r'^increment-decrement-score', views.increment_decrement_score),
+    url(r'^matches', views.show_all_matches),
+    url(r'^table', views.table),
+    url(r'^', views.main_web),
+
 ]
